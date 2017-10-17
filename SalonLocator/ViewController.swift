71524d74906a30b759955a7fb8e2663b83f2ea35
Denys Astapov <cr0we@icloud.com>
2017-10-17 23:18:28 +0300

    Initial Commit

---
+++ SalonLocator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SalonLocator
+//
+//  Created by Denys Astapov on 17.10.2017.
+//  Copyright © 2017 Denys Astapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
